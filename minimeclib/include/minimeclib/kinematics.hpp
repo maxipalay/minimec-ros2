5d3dd759fa6d5d2d7537a2259a97dafc8dc4b5d1
Maximiliano Palay <maximilianopalay@gmail.com>
2024-02-23 20:33:37 -0600

    added forward kinematics/odometry calculation

--- minimeclib/include/minimeclib/kinematics.hpp
+++ minimeclib/include/minimeclib/kinematics.hpp
@@ -3,10 +3,27 @@
 /// @file
 /// @brief Kinematics for a mecanum wheel base.
 
+#include "tf2/LinearMath/Quaternion.h"
+#include "tf2/LinearMath/Transform.h"
+#include "tf2/LinearMath/Vector3.h"
+#include <iostream>
+
 namespace minimeclib {
 
     /// @brief struct containing the wheel speeds
     struct WheelSpeeds {
+        /// @brief front-left
+        double fl{};
+        /// @brief front-right
+        double fr{};
+        /// @brief rear-right
+        double rr{};
+        /// @brief rear-left
+        double rl{};
+    };
+
+    /// @brief struct representing wheel positions
+    struct WheelPositions {
         /// @brief front-left
         double fl{};
         /// @brief front-right
@@ -25,6 +42,8 @@ namespace minimeclib {
             double l;
             /// @brief wheel radius (meters)
             double r;
+            /// @brief keep track of wheel positions between calls
+            WheelPositions wheel_positions;
 
         public:
             /// @brief default constructor
@@ -42,6 +61,17 @@ namespace minimeclib {
             /// @param vy - twist y velocity
             /// @return wheel speeds
             WheelSpeeds IKin(const double wz, const double vx, const double vy);
+
+            /// @brief Get the relative transform form the previous wheel angles
+            /// @param positions - wheel positions (radians)
+            /// @return the relative transform
+            tf2::Transform FKin(const WheelPositions new_wheel_positions);
+
+            /// @brief Set the initial position of the wheels
+            /// @param initial_positions - WheelPositions
+            void setWheelOffsets(const WheelPositions initial_positions);
+
+
     };
 
 
--- minimeclib/src/kinematics.cpp
+++ minimeclib/src/kinematics.cpp
@@ -11,11 +11,43 @@ namespace minimeclib {
 
     WheelSpeeds MecanumDrive::IKin(const double wz, const double vx, const double vy)
     {
-        double fl = 1/r*((-l-w)*wz+vx-vy);
-        double fr = 1/r*((l+w)*wz+vx+vy);
-        double rr = 1/r*((l+w)*wz+vx-vy);
-        double rl = 1/r*((-l-w)*wz+vx+vy);
+        double fl = 1.0/r*((-l-w)*wz+vx-vy);
+        double fr = 1.0/r*((l+w)*wz+vx+vy);
+        double rr = 1.0/r*((l+w)*wz+vx-vy);
+        double rl = 1.0/r*((-l-w)*wz+vx+vy);
         return {fl, fr, rr, rl};
+    }
+
+    tf2::Transform MecanumDrive::FKin(const WheelPositions new_positions)
+    {
+        auto yaw = r/4.0*
+                    (-1.0/(l+w)*(new_positions.fl-wheel_positions.fl)+
+                      1.0/(l+w)*(new_positions.fr-wheel_positions.fr)+
+                      1.0/(l+w)*(new_positions.rr-wheel_positions.rr)-
+                      1.0/(l+w)*(new_positions.rl-wheel_positions.rl));
+        
+        auto dx = r/4.0*
+                    ((new_positions.fl-wheel_positions.fl)+
+                     (new_positions.fr-wheel_positions.fr)+
+                     (new_positions.rr-wheel_positions.rr)+
+                     (new_positions.rl-wheel_positions.rl));
+        
+        auto dy = r/4.0*
+                    (-(new_positions.fl-wheel_positions.fl)+
+                      (new_positions.fr-wheel_positions.fr)-
+                      (new_positions.rr-wheel_positions.rr)+
+                      (new_positions.rl-wheel_positions.rl));
+
+        auto translation = tf2::Vector3(dx, dy, 0.0);
+        auto rotation = tf2::Quaternion{};
+        rotation.setRPY(0.0, 0.0, yaw);
+        wheel_positions = new_positions;
+        return tf2::Transform(rotation, translation);
+    }
+
+    void MecanumDrive::setWheelOffsets(const WheelPositions new_positions)
+    {
+        wheel_positions = new_positions;
     }
 
 }
