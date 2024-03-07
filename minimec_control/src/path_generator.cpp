e6625f7546b5e1999c2e7d27f7230b32a85bb487
Maximiliano Palay <maximilianopalay@gmail.com>
2024-03-06 21:25:05 -0600

    add circular path generation functionality

--- minimec_control/src/path_generator.cpp
+++ minimec_control/src/path_generator.cpp
@@ -4,11 +4,13 @@
 #include <string>
 
 #include "rclcpp/rclcpp.hpp"
-#include "minimec_msgs/srv/splne_plan_request.hpp"
+#include "minimec_msgs/srv/spline_plan_request.hpp"
+#include "minimec_msgs/srv/circular_plan_request.hpp"
 #include "geometry_msgs/msg/pose_stamped.hpp"
 #include "nav_msgs/msg/path.hpp"
 #include "visualization_msgs/msg/marker_array.hpp"
 #include "tinysplinecxx.h"
+#include "tf2/utils.h"
 
 using namespace std::chrono_literals;
 
@@ -22,10 +24,15 @@ public:
     auto qos_ = rclcpp::SystemDefaultsQoS{};
     qos_.reliable();
 
-    path_service_ =
+    spline_path_service_ =
       create_service<minimec_msgs::srv::SplinePlanRequest>(
       "generate_spline_plan",
-      std::bind(&PathGenerator::planCallback, this, std::placeholders::_1, std::placeholders::_2));
+      std::bind(&PathGenerator::splinePlanCallback, this, std::placeholders::_1, std::placeholders::_2));
+
+    circular_path_service_ =
+      create_service<minimec_msgs::srv::CircularPlanRequest>(
+      "generate_circular_plan",
+      std::bind(&PathGenerator::circularPlanCallback, this, std::placeholders::_1, std::placeholders::_2));
 
     path_publisher_ = create_publisher<nav_msgs::msg::Path>("plan", qos_);
 
@@ -36,7 +43,48 @@ public:
 
 private:
 
-    void planCallback(const std::shared_ptr<minimec_msgs::srv::SplinePlanRequest::Request> request,
+    void circularPlanCallback(const std::shared_ptr<minimec_msgs::srv::CircularPlanRequest::Request> request,
+    std::shared_ptr<minimec_msgs::srv::CircularPlanRequest::Response> response){
+        std::string frame_id = request->frame_id;
+        double angle_resolution = request->resolution;
+        double r = request->radius;
+
+        // our generated path
+        nav_msgs::msg::Path generated_path = nav_msgs::msg::Path();
+        generated_path.header.frame_id = frame_id; 
+        
+        double c_x = request->center.x;
+        double c_y = request->center.y;
+
+        // number of points in the circular path
+        int steps = static_cast<int>(2.0*3.14159/angle_resolution);
+        for (int i = 0; i < steps; i++){
+            auto pose = geometry_msgs::msg::PoseStamped();
+
+            pose.pose.position.x = c_x + r*std::cos(static_cast<double>(i)/steps*2.0*3.14159-3.14159/2.0);//-3.14159/2.0
+            pose.pose.position.y = c_y + r*std::sin(static_cast<double>(i)/steps*2.0*3.14159-3.14159/2.0);
+            auto rot = tf2::Quaternion();
+            // get the angle from our current position to the point
+            auto angle = std::atan2(pose.pose.position.y-c_y, pose.pose.position.x-c_x);
+            rot.setRPY(0.0, 0.0, angle);
+            pose.pose.orientation.x = rot.getX();
+            pose.pose.orientation.y = rot.getY();
+            pose.pose.orientation.z = rot.getZ();
+            pose.pose.orientation.w = rot.getW();
+            generated_path.poses.insert(
+                generated_path.poses.end(),
+                pose);
+        }
+
+        // publish
+        path_publisher_->publish(generated_path);
+
+        // set path in service response
+        response->path = generated_path;
+
+    }
+
+    void splinePlanCallback(const std::shared_ptr<minimec_msgs::srv::SplinePlanRequest::Request> request,
     std::shared_ptr<minimec_msgs::srv::SplinePlanRequest::Response> response){
         // get frame_id from the message
         std::string frame_id = request->frame_id;
@@ -84,11 +132,11 @@ private:
             n_markers++;
         }
         
-        if (msg->heading_mode == "point"){
+        if (request->heading_mode == "point"){
             visualization_msgs::msg::Marker marker;
             marker.header.frame_id = frame_id;
             marker.header.stamp = get_clock()->now();
-            marker.id = i;
+            marker.id = n_markers;
             marker.type = visualization_msgs::msg::Marker::SPHERE;
             marker.action = visualization_msgs::msg::Marker::ADD;
             marker.pose.position.x = request->heading_point.x;
@@ -136,11 +184,11 @@ private:
             pose.pose.position.x = static_cast<double>(result.at(0));
             pose.pose.position.y = static_cast<double>(result.at(1));
             auto rot = tf2::Quaternion();
-            if (msg->heading_mode == "fixed"){
-                rot.setRPY(0.0, 0.0, msg->heading_angle);
-            } else if (msg->heading_mode == "point") {
+            if (request->heading_mode == "fixed"){
+                rot.setRPY(0.0, 0.0, request->heading_angle);
+            } else if (request->heading_mode == "point") {
                 // get the angle from our current position to the point
-                auto angle = std::atan2(msg->heading_point.y-pose.pose.position.y, msg->heading_point.x-pose.pose.position.x);
+                auto angle = std::atan2(request->heading_point.y-pose.pose.position.y, request->heading_point.x-pose.pose.position.x);
                 rot.setRPY(0.0, 0.0, angle);
             }
             pose.pose.orientation.x = rot.getX();
@@ -160,7 +208,8 @@ private:
         response->path = generated_path;
     }
     
-    rclcpp::Service<minimec_msgs::srv::SplinePlanRequest>::SharedPtr path_service_;
+    rclcpp::Service<minimec_msgs::srv::SplinePlanRequest>::SharedPtr spline_path_service_;
+    rclcpp::Service<minimec_msgs::srv::CircularPlanRequest>::SharedPtr circular_path_service_;
     rclcpp::Publisher<nav_msgs::msg::Path>::SharedPtr path_publisher_;
     rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr markers_publisher_;
   
