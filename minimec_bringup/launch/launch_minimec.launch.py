bf489ca3dde9637c79ace2bd01ca0d830cad4dac
Maximiliano Palay <maximilianopalay@gmail.com>
2024-03-06 19:14:36 -0600

    update launch files, separate launch files for launching everything on robot and on computer

--- minimec_bringup/launch/launch_minimec.launch.py
+++ minimec_bringup/launch/launch_minimec.launch.py
@@ -5,11 +5,16 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.substitutions import FindPackageShare
 from launch_ros.actions import Node
 from launch_ros.substitutions import ExecutableInPackage, FindPackageShare
-from launch.substitutions import Command, PathJoinSubstitution
-
+from launch.substitutions import Command, PathJoinSubstitution, LaunchConfiguration
+from launch.actions.declare_launch_argument import DeclareLaunchArgument
 
 def generate_launch_description():
     return LaunchDescription([
+        DeclareLaunchArgument(
+            'cmd_src',
+            default_value='teleop',
+            description="controls the source of the command velocity commands.",
+            choices=["teleop", "path"]),
         IncludeLaunchDescription(
             YAMLLaunchDescriptionSource(
                 [FindPackageShare("odrive_can"), '/launch',
@@ -27,6 +32,7 @@ def generate_launch_description():
                 [FindPackageShare("minimec_driver"), '/launch',
                  '/launch_driver.launch.py']
             ),
+            launch_arguments=[['cmd_src', LaunchConfiguration('cmd_src')]]
         ),
         Node(
             package='robot_state_publisher',
